613ef0bd6784ab85025b8348d46930b5692f9ab3
ahmed <ahmed@baid.com>
2025-02-13 17:49:45 +0100

    add the style of carddetail moving to responsive

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,24 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 
 	"groupie/handler"
+	tools "groupie/tools"
 )
 
 func Route() {
+	var err error
+
+	// parse all the html file from the template folder to variable Tp
+	tools.Tp, err = template.ParseGlob("template/*.html")
+	if err != nil {
+		fmt.Println("Error parsing templates: ", err)
+		return
+	}
+	// our handlers
 	http.HandleFunc("/static/", handler.Style_Func)
 	http.HandleFunc("/details/", handler.Detail_Card_Func)
 	http.HandleFunc("/", handler.Groupie_Func)
