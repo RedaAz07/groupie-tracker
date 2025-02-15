29156b19a69617ce84b7036400c0060b1dabbdac
ahmed <ahmed@baid.com>
2025-02-15 11:21:14 +0100

    update struct

--- tools/Tools.go
+++ tools/Tools.go
@@ -5,8 +5,6 @@ import (
 	"text/template"
 )
 
-var Tp *template.Template
-
 // our data structure
 type (
 	ErrorPage struct {
@@ -40,23 +38,25 @@ type (
 )
 
 // NewErrorPage creates a new ErrorPage
-var ErrorBadReq = ErrorPage{
-	Code:         http.StatusBadRequest,
-	ErrorMessage: "Oops! It looks like there was an issue with your request. Please check your input and try again.",
-}
+var (
+	Tp          *template.Template
+	ErrorBadReq = ErrorPage{
+		Code:         http.StatusBadRequest,
+		ErrorMessage: "Oops! It looks like there was an issue with your request. Please check your input and try again.",
+	}
 
-var ErrorNotFound = ErrorPage{
-	Code:         http.StatusNotFound,
-	ErrorMessage: "Uh-oh! The page you're looking for doesn't exist. It might have been moved or deleted.",
-}
+	ErrorNotFound = ErrorPage{
+		Code:         http.StatusNotFound,
+		ErrorMessage: "Uh-oh! The page you're looking for doesn't exist. It might have been moved or deleted.",
+	}
 
-var ErrorMethodnotAll = ErrorPage{
-	Code:         http.StatusMethodNotAllowed,
-	ErrorMessage: "The request method is not supported for this resource. Please check and try again with a valid method.",
-}
+	ErrorMethodnotAll = ErrorPage{
+		Code:         http.StatusMethodNotAllowed,
+		ErrorMessage: "The request method is not supported for this resource. Please check and try again with a valid method.",
+	}
 
-var ErrorInternalServerErr = ErrorPage{
-	Code:         http.StatusInternalServerError,
-	ErrorMessage: "Something went wrong on our end. We're working on fixing it—please try again later!",
-}
-
+	ErrorInternalServerErr = ErrorPage{
+		Code:         http.StatusInternalServerError,
+		ErrorMessage: "Something went wrong on our end. We're working on fixing it—please try again later!",
+	}
+)
